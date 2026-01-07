48c1de9d8d5eddc72dd2d5c24d43b69f3a016f3d
Artem <alefa2000@mail.ru>
2026-01-08 00:33:55 +0300

    Update Validators.kt

--- develop/application/src/main/java/com/example/application/common/utils/Validators.kt
+++ develop/application/src/main/java/com/example/application/common/utils/Validators.kt
@@ -2,5 +2,6 @@ package com.example.application.common.utils
 
 
 fun String.isEmailValid () : Boolean {
-    return true
+    val emailRegex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}\$".toRegex()
+    return this.matches(emailRegex)
 }
